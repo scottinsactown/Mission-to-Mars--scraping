f7cdebb695817e8f23a67acd267b8a2d086f214d
Scott Clark <scottinsacto@yahoo.com>
2019-12-15 13:42:50 -0800

    image done

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,10 @@
+#Load dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_costa
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, url="mongodb://localhost:27017/weather_app")
