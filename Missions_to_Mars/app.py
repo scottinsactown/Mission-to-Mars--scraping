b790e14a69d77c5036b7b4428c08298a120c71c9
Scott Clark <scottinsacto@yahoo.com>
2019-12-16 06:33:35 -0800

    start to flask/mongo

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,4 +1,3 @@
-#Load dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_costa
@@ -7,4 +6,27 @@ import scrape_costa
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, url="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    planet_data = mongo.db.collection.find_all()
+    return render_template("index.html", mars = planet_data) #variable was vacation
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
